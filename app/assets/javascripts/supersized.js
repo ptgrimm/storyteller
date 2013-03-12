9cb93e4b121b52b56ac8414921defb52216b45c8
ptgrimm <ptgrimm@gmail.com>
2013-03-12 10:46:10 -0700

    icons, slides updated

--- app/assets/javascripts/supersized.js
+++ app/assets/javascripts/supersized.js
@@ -4,7 +4,7 @@ jQuery(function($){
      slide_interval  :   5700,  // Length between transitions
      transition  :   1,   // 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
      transition_speed  :  3600,  // Speed of transition
-     stop_loop   :   0, 
+     stop_loop   :   1, 
      random  :   0,
      vertical_center   :   0,
      // Components  
@@ -13,7 +13,6 @@ jQuery(function($){
      {image : 'assets/universe.gif ', title : '', thumb : '', url : ''},
      {image : 'assets/storyteller-is.gif', title : '', thumb : '', url : ''},
      {image : 'assets/find-out.gif', title : '', thumb : '', url : ''},
-     {image : 'assets/krishna2.gif', title : '', thumb : '', url : ''},
      ]
     });
 });
